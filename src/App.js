14bebfa54b4376029f01eeb62d965b419d4334a1
DevAleksandarS <spasojevic.dev@gmail.com>
2022-08-31 17:41:26 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import "./css/style.css";
+import AddCards from "./components/AddCards";
+import FlashCards from "./components/FlashCards";
+import DeleteCard from "./components/DeleteCard";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_CARDS_BLOCK":
+      return {
+        addCards: "visible",
+        flashcards: "non-visible",
+        deleteCard: "non-visible",
+      };
+    case "FLASHCARDS_BLOCK":
+      return {
+        addCards: "non-visible",
+        flashcards: "visible",
+        deleteCard: "non-visible",
+      };
+    case "DELETE_CARD_BLOCK":
+      return {
+        addCards: "non-visible",
+        flashcards: "non-visible",
+        deleteCard: "visible",
+      };
+    case "CLOSE":
+      return {
+        addCards: "non-visible",
+        flashcards: "non-visible",
+        deleteCard: "non-visible",
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    addCards: "non-visible",
+    flashcards: "non-visible",
+    deleteCard: "non-visible",
+  });
+
+  const [cards, setCards] = useState([]);
+
+  function addCard(question, answer) {
+    setCards([...cards, { question: question, answer: answer }]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="btn-cont">
+        <button
+          onClick={() => {
+            dispatch({ type: "ADD_CARDS_BLOCK" });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add Cards
+        </button>
+        <button
+          onClick={() => {
+            if (cards.length <= 3) {
+              alert("You need to create at least 4 cards!");
+            } else {
+              dispatch({ type: "FLASHCARDS_BLOCK" });
+            }
+          }}
+        >
+          Start Flashcards
+        </button>
+        <button
+          onClick={() => {
+            setCards([]);
+          }}
+        >
+          Delete All Cards
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "DELETE_CARD_BLOCK" });
+          }}
+        >
+          Delete Card
+        </button>
+      </div>
+
+      <AddCards
+        addCard={addCard}
+        visibility={state.addCards}
+        dispatch={dispatch}
+      />
+      <FlashCards
+        cards={cards}
+        visibility={state.flashcards}
+        dispatch={dispatch}
+      />
+      <DeleteCard
+        cards={cards}
+        visibility={state.deleteCard}
+        dispatch={dispatch}
+      />
     </div>
   );
 }
