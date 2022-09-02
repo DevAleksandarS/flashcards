f87b31fd5cb629190a543e7a87cccd8d34a988c8
DevAleksandarS <spasojevic.dev@gmail.com>
2022-09-02 22:43:18 +0200

    Initial commit

--- src/components/FlashCards.js
+++ src/components/FlashCards.js
@@ -1,48 +1,7 @@
-import React, { useRef } from "react";
-import FlashCardsDisplay from "./FlashCardsDisplay";
+import React from "react";
 
 function FlashCards({ cards, visibility, dispatch }) {
-  const nextCardRef = useRef();
-
-  return (
-    <>
-      <div
-        onClick={() => {
-          dispatch({ type: "CLOSE" });
-        }}
-        className={`black-bg ${visibility}`}
-      ></div>
-      <div className={`block ${visibility}`}>
-        <FlashCardsDisplay ref={nextCardRef} cards={cards} />
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            nextCardRef.current.correctBtn();
-          }}
-          className="btn"
-        >
-          Correct
-        </button>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            nextCardRef.current.incorrectBtn();
-          }}
-          className="btn margin-l"
-        >
-          Incorrect
-        </button>
-        <button
-          onClick={() => {
-            dispatch({ type: "CLOSE" });
-          }}
-          className="btn margin-l"
-        >
-          Close
-        </button>
-      </div>
-    </>
-  );
+  return null;
 }
 
 export default FlashCards;
